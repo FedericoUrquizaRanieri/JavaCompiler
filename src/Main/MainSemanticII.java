85c37539d2faba562e4e417d67ed50deff71d9a8
Federico Urquiza <federicourquizaranieri@gmail.com>
2025-10-14 10:59:59 -0300

    Adding base to start working

---
+++ src/Main/MainSemanticII.java
@@ -0,0 +1,68 @@
+package Main;
+
+import Lexical.Analyzer.LexicalAnalyzer;
+import Lexical.SpecialWordMap.SpecialWordsMap;
+import Semantic.SemExceptions.SemanticException;
+import Semantic.SymbolTable;
+import SourceManager.SourceManager;
+import SourceManager.SourceManagerImpl;
+import Syntactic.Analyzer.ProductionsMap;
+import Syntactic.Analyzer.SyntacticAnalyzer;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class MainSemanticII {
+    public static SymbolTable symbolTable;
+
+    public static void main(String[] args) {
+        SourceManager sourceManager = new SourceManagerImpl();
+        try {
+            sourceManager.open(args[0]);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        SpecialWordsMap specialWordsMap = new SpecialWordsMap();
+        symbolTable = new SymbolTable();
+        ProductionsMap productionsMap = new ProductionsMap();
+        LexicalAnalyzer lexicalAnalyzer = new LexicalAnalyzer(sourceManager, specialWordsMap);
+        SyntacticAnalyzer syntacticAnalyzer = new SyntacticAnalyzer(lexicalAnalyzer, productionsMap);
+
+        boolean noMistakes = true;
+        try {
+            syntacticAnalyzer.startAnalysis();
+        } catch (CompiException e) {
+            e.printError();
+            noMistakes=false;
+        }
+
+        if (noMistakes) {
+            try {
+                symbolTable.checkStatements();
+            } catch (SemanticException e) {
+                e.printError();
+                noMistakes=false;
+            }
+        }
+
+        if (noMistakes) {
+            try {
+                symbolTable.consolidate();
+            } catch (SemanticException e) {
+                e.printError();
+                noMistakes=false;
+            }
+        }
+        if (noMistakes) {
+            System.out.println("Compilacion Exitosa");
+            System.out.println();
+            System.out.println("[SinErrores]");
+        }
+
+        try {
+            sourceManager.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
